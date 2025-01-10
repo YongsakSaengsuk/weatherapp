cc0565e3ffd1dae191fae7545c3f9c1ad31020f9
YongsakSaengsuk <Yongsaksaengsuk@gmail.com>
2025-01-10 12:57:14 +0700

    first_page

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const WeatherApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class WeatherApp extends StatelessWidget {
+  const WeatherApp({super.key});
 
-
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.blue, Colors.white],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
+          ),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SvgPicture.asset(
+                  "assets/images/cloudy_sunny.svg",
+                  height: 200,
+                ),
+                Text(
+                  "Weather Forecasts",
+                  style: TextStyle(fontSize: 30),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                    onPressed: () {}, child: const Text("Get Start")),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
